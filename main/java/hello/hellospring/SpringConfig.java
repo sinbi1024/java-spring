b1cc169dd6f556e83df18fffeaceaa33ccc777ef
sinbi1024 <snbiiiii1024@gmail.com>
2022-02-14 02:12:38 +0900

    settings

--- main/java/hello/hellospring/SpringConfig.java
+++ main/java/hello/hellospring/SpringConfig.java
@@ -6,8 +6,16 @@ import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+      return new MemoryMemberRepository();
     }
 
 }
