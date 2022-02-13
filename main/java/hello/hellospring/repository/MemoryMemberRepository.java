a11049709702c03a4343f477bc837dd05f9945fb
sinbi1024 <snbiiiii1024@gmail.com>
2022-02-13 14:42:25 +0900

    repository test

--- main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -37,4 +37,8 @@ public class MemoryMemberRepository implements MemberRepository {
         return new ArrayList<>(store.values());
     }
 
+    public void clearStore() {
+        store.clear();
+    }
+
 }
