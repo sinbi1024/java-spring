a1e8ba3e0ff05fa18cb6a9ef4f840ba02e69b431
sinbi1024 <snbiiiii1024@gmail.com>
2022-02-16 23:36:28 +0900

    AOP

--- main/java/hello/hellospring/SpringConfig.java
+++ main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,9 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
